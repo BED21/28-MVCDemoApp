0a23710c480afba5befdb0f59f4594f64b4f8049
Claes Engelin <claes.engelin@zocom.se>
2022-03-09 12:06:11 +0100

    Lagt till en ny Controller och en ny View
    Nu kan man gå till /First/Hello

---
+++ MVCDemo/Controllers/FirstController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.Controllers;
+
+public class FirstController : Controller
+{
+    public string Index()
+    {
+        //return View();
+        return "Hello World!!";
+    }
+
+    public IActionResult Hello()
+    {
+        return View();
+    }
+}
