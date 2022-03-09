e85ef367926b12bd652f4496a54d36f0ac0cdc5f
Claes Engelin <claes.engelin@zocom.se>
2022-03-09 13:22:42 +0100

    Added dynamic data to views

--- MVCDemo/Controllers/FirstController.cs
+++ MVCDemo/Controllers/FirstController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using MVCDemo.Models;
+
 namespace MVCDemo.Controllers;
 
 public class FirstController : Controller
@@ -12,6 +14,20 @@ public class FirstController : Controller
 
     public IActionResult Hello()
     {
+        ViewBag.Message = "Hello world";
         return View();
+    }
+
+    public IActionResult Info()
+    {
+        Person person = new Person()
+        {
+            Id = 1,
+            Name = "Claes",
+            Age = 65,
+            Location = "Göteborg"
+        };
+
+        return View(person);
     }
 }
---
+++ MVCDemo/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCDemo.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public int Age { get; set; }
+    public string Location { get; set; } = String.Empty;
+}
